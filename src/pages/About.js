442294e2a9afc99eb332e8949b724f932796f957
Ericmohn <eric_mohn@hotmail.com>
2023-06-29 21:12:08 -0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Aliqua exercitation proident dolor excepteur eiusmod reprehenderit laboris mollit laborum veniam veniam. 
+        Esse officia laboris fugiat anim aliquip pariatur occaecat irure fugiat velit in velit. 
+        Eu esse elit dolore nisi labore laboris voluptate in deserunt exercitation pariatur commodo et occaecat. 
+        Voluptate ullamco dolor ullamco magna Lorem ad.
+      </p>
+    </section>
+  )
+}
+
+export default About
